6b309e42301a478a347b4c9aa547b1039d5ec6e2
Anwesh43 <anweshthecool0@gmail.com>
2021-09-12 13:44:10 +0530

    adding DrawingUtil class

--- index.ts
+++ index.ts
@@ -26,3 +26,51 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawClippedCircle(context : CanvasRenderingContext2D, x : number, y : number, r : number, sc : number) {
+        context.save()
+        context.beginPath()
+        context.arc(x, y, r, 0, 2 * Math.PI)
+        context.clip()
+        context.fillRect(x - r, y - r, 2 * r * sc, 2 * r)
+        context.restore()
+    }
+
+    static drawLineExpandBall(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const r : number = Math.min(w, h) / rFactor 
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const sc4 : number = ScaleUtil.divideScale(scale, 3, parts)
+        context.save()
+        context.translate(w / 2, h / 2)
+        for (var j = 0; j < 2; j++) {
+            context.save()
+            context.scale(1 - 2 * j, 1)
+            context.translate(0, h / 2 * (1 - 2 * j) * sc4)
+            context.rotate(deg * sc3)
+            DrawingUtil.drawLine(context, 0, 0, 0, size * sc1)
+            DrawingUtil.drawClippedCircle(context, size + r, 0, r, sc2)
+            context.restore()
+        }
+        context.restore()
+    }
+
+    static drawLEBNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.fillStyle = colors[i]
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawLineExpandBall(context, scale)
+    }
+}
