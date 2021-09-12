b45165c1983cea368dc7d25b92773aa60e6e970d
Anwesh43 <anweshthecool0@gmail.com>
2021-09-12 14:21:22 +0530

    adding LineExpandBall class

--- index.ts
+++ index.ts
@@ -191,3 +191,26 @@ class LEBNode {
         return this
     }
 }
+
+class LineExpandBall {
+
+    curr : LEBNode = new LEBNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUdpating(cb)
+    }
+}
